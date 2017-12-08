407dd88c906ccae7fc45d2d05f65415d793b840e
Hung Lun <hunglun.o@gmail.com>
2017-12-08 19:43:37 +0800

    fix isFull bug

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -9,7 +9,7 @@
  *  This program estimates the value of the percolation threshold via Monte Carlo simulation
  *
  ******************************************************************************/
-
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Out;
 import edu.princeton.cs.algs4.StdRandom;
@@ -29,71 +29,94 @@ public class Percolation {
      * @throws IllegalArgumentException if n is 0 or less
      */
     public Percolation(int n){
-	if (n <= 0)
-            throw new IllegalArgumentException("n is 0 or less");
-	uf = new WeightedQuickUnionUF(n*n+2);
-	this.count = 0;
-	this.n = n;
-
-	this.grid = new boolean[n][n]; // comply with the convention of col and row indices
-	for(int i=0; i<n; i++){
-	    for(int j=0; j<n; j++){
-		grid[i][j] = false; // false : blocked, true : open
-	    }
-	}
-
-	// connect top virtual site with the top row
-	// let n*n be the top virtual site
-	for(int i=0; i<n; i++){
-	    uf.union(n*n,i);
-	}
-	// connect bottom virtual site with the bottom row
-	// let n*n+1 be the bottom virtual site
-	for(int i=0; i<n; i++){
-	    uf.union(n*n+1,n*(n-1)+i);
-	}
-
+      if (n <= 0)
+        throw new IllegalArgumentException("n is 0 or less");
+      uf = new WeightedQuickUnionUF(n*n+2);
+      this.count = 0;
+      this.n = n;
+      
+      this.grid = new boolean[n][n]; // comply with the convention of col and row indices
+      for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+          grid[i][j] = false; // false : blocked, true : open
+        }
+      }
+      
+      // connect top virtual site with the top row
+      // let n*n be the top virtual site
+      for(int i=0; i<n; i++){
+        uf.union(n*n,i);
+      }
+      // connect bottom virtual site with the bottom row
+      // let n*n+1 be the bottom virtual site
+      for(int i=0; i<n; i++){
+        uf.union(n*n+1,n*(n-1)+i);
+      }
     }
     public    void open(int row, int col) {    // open site (row, col) {} if it is not open already
-	row = row - 1;
-	col = col - 1;
-	if (this.grid[row][col] == false){
-	    this.grid[row][col] = true;
-	    this.count++;
-	}
-	// union with neighbouring sites
-	for(int k=-1;k<1;k++){
-	    for(int i=-1;i<1;i++){
-		if (row+i < 0 || col+k <0) continue;
-		if(this.grid[row+i][col+k]){
-		    // Check what happens with union(m,m) ?
-		    uf.union(row*this.n+col, (row+i)*this.n+(col+k));
-		}
-	    }
-	}
-	// uf.union(row,col);
-	// uf.find(0);
-	// uf.connected(0,1);
-	// uf.count();
+      row = row - 1;
+      col = col - 1;
+      if (this.grid[row][col] == false){
+        this.grid[row][col] = true;
+        this.count++;
+      }
+      // union with neighbouring sites
+      for(int k=-1;k<2;k++){
+        for(int i=-1;i<2;i++){
+          if (row+i < 0 || col+k <0) continue;
+          if (row+i > this.n-1 || col+k >this.n-1) continue;
+          if(this.grid[row+i][col+k] && this.grid[row][col]){
+            // Check what happens with union(m,m) ?
+            uf.union(row*this.n+col, (row+i)*this.n+(col+k));
+          }
+        }
+      }
+      // uf.union(row,col);
+      // uf.find(0);
+      // uf.connected(0,1);
+      // uf.count();
     }
     public boolean isOpen(int row, int col) {   // is site (row, col) open?
-	row = row - 1;
-	col = col - 1;
-	return this.grid[row][col];
+      row = row - 1;
+      col = col - 1;
+      return this.grid[row][col];
     }
     public boolean isFull(int row, int col) {  // is site (row, col) full?
-	row = row - 1;
-	col = col - 1;
-	return uf.connected(n*n,row*this.n+col);
+      if (isOpen(row,col)){
+        row = row - 1;
+        col = col - 1;
+        return uf.connected(n*n,row*this.n+col);
+      }
+      return false;
     }
     public int numberOfOpenSites() {       // number of open sites
-	return this.count;
+      return this.count;
     }
     public boolean percolates() {              // does the system percolate?
-	return uf.connected(n*n,n*n+1);
+      return uf.connected(n*n,n*n+1);
     }
-
-
     public static void main(String[] args) { // this is optional
+      Percolation perc = new Percolation(5);
+      for(int k=-1;k<2;k++){
+        StdOut.println(k);
+      }
+      assert(perc.numberOfOpenSites() == 0);
+      assert(perc.isFull(1,1) == false);
+      perc.open(1,1);
+      assert(perc.isFull(1,1));
+      
+      assert(perc.isFull(2,1) == false);
+      perc.open(2,1);
+      assert(perc.isOpen(2,1));
+      assert(perc.isFull(2,1));
+      
+      perc.open(3,1);
+      perc.open(4,1);
+      perc.open(5,1);
+      assert(perc.isFull(5,1));
+      
+   
+      assert(perc.percolates());
+      StdOut.println("End Percolation Program");
     }
 }
--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -5,5 +5,7 @@ public class PercolationStats {
    public double confidenceLo(){return 0;}                  // low  endpoint of 95% confidence interval
    public double confidenceHi(){return 0;}                  // high endpoint of 95% confidence interval
 
-   public static void main(String[] args){}        // test client (described below)
+   public static void main(String[] args){
+       
+   }        // test client (described below)
 }
