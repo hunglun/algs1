9e49059052ea4f43ced256da99cabaeaea8a3ba7
Hung Lun <hunglun.o@gmail.com>
2017-12-10 12:08:37 +0800

    implemented random remove

--- queues/RandomizedQueue.java
+++ queues/RandomizedQueue.java
@@ -1,5 +1,8 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
 public class RandomizedQueue<Item> implements Iterable<Item> {
   private Node<Item> first;    // beginning of queue
   private Node<Item> last;     // end of queue
@@ -48,13 +51,48 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     else           oldlast.next = last;
     n++;
   }
-  
+  private Item removeFirst(){
+    Item item = first.item;
+        first = first.next;
+        n--;
+        if (isEmpty()) last = null;   // to avoid loitering
+        StdOut.println("Random number is 0");
+        StdOut.println("Removed item is "  + item);
+        return item;
+  }
   public Item dequeue()                    // remove and return a random item
   {
     if (isEmpty()) throw new NoSuchElementException("Queue underflow");
     
+    Node<Item> discarded;
+    Node<Item> prev = null;
+    int randNum = StdRandom.uniform(0,size()); // get a random number between 0 and size - 1
+    if (randNum == 0) return removeFirst();
+    // use iterator and find randNum th element in the queue
+    // remove it
+    StdOut.println("Random number is " + randNum);
+    discarded = first;
+    for(Node<Item> node=first; randNum>0; node = node.next){
+ //     StdOut.println(node.item);
+      prev = node;
+      discarded = node.next;
+      randNum--;
+    }
     
-    return null;
+    Item item = discarded.item; 
+    if (prev != null)
+      prev.next = discarded.next;
+    discarded.next = null;
+    n--;
+    if (isEmpty()){ 
+      last = null;   // to avoid loitering
+      first = null;
+    }
+    
+//    StdOut.println();
+    StdOut.println("Removed item is "  + item);
+    return item;
+    
   }
   public Item sample()                     // return a random item (but do not remove it)
   {
@@ -91,6 +129,41 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
   }
   public static void main(String[] args)   // unit testing (optional)
-  {}
+  {
+    RandomizedQueue<String> queue = new RandomizedQueue<String>();
+    queue.enqueue("0");
+    queue.enqueue("1");
+    queue.enqueue("2");
+    queue.enqueue("3");
+    queue.enqueue("4");
+    
+    queue.dequeue();
+    queue.dequeue();
+    queue.dequeue();
+    queue.dequeue();
+    queue.dequeue();
+    assert(queue.isEmpty());
+    /*
+    for(String s : queue){
+      StdOut.println(s);
+    }
+    queue.dequeue();
+    
+    for(String s : queue){
+      StdOut.println(s);
+    }
+    queue.dequeue();
+    for(String s : queue){
+      StdOut.println(s);
+    }
+    queue.dequeue();
+    for(String s : queue){
+      StdOut.println(s);
+    }
+    queue.dequeue();
+    for(String s : queue){
+      StdOut.println(s);
+    }*/
+  }
 }
 
